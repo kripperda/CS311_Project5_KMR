5f8a06b8b8434ece05d1e0e227b40a5021eb1d9c
Kory Ripperda <kripperda@bellarmine.edu>
2024-11-18 13:31:37 -0600

    Add files via upload

---
+++ js/MadLib.js
@@ -0,0 +1,98 @@
+function submit() {
+    let adjective1InputElement = document.getElementById('adjective1Input');
+    let adjective1 = adjective1InputElement.value;
+    let adjective1OutputElement = document.getElementById('adjective1Output');
+    adjective1OutputElement.innerText = adjective1;
+
+    let adjective2InputElement = document.getElementById('adjective2Input');
+    let adjective2 = adjective2InputElement.value;
+    let adjective2OutputElement = document.getElementById('adjective2Output');
+    adjective2OutputElement.innerText = adjective2;
+
+    let noun1InputElement = document.getElementById('noun1Input');
+    let noun1 = noun1InputElement.value;
+    let noun1OutputElement = document.getElementById('noun1Output');
+    noun1OutputElement.innerText = noun1;
+
+    let noun2InputElement = document.getElementById('noun2Input');
+    let noun2 = noun2InputElement.value;
+    let noun2OutputElement = document.getElementById('noun2Output');
+    noun2OutputElement.innerText = noun2;
+
+    let pluralNoun1InputElement = document.getElementById('pluralNoun1Input');
+    let pluralNoun1 = pluralNoun1InputElement.value;
+    let pluralNoun1OutputElement = document.getElementById('pluralNoun1Output');
+    pluralNoun1OutputElement.innerText = pluralNoun1;
+
+    let gameInputElement = document.getElementById('gameInput');
+    let game = gameInputElement.value;
+    let gameOutputElement = document.getElementById('gameOutput');
+    gameOutputElement.innerText = game;
+
+    let pluralNoun2InputElement = document.getElementById('pluralNoun2Input');
+    let pluralNoun2 = pluralNoun2InputElement.value;
+    let pluralNoun2OutputElement = document.getElementById('pluralNoun2Output');
+    pluralNoun2OutputElement.innerText = pluralNoun2;
+
+    let ingVerb1InputElement = document.getElementById('ingVerb1Input');
+    let ingVerb1 = ingVerb1InputElement.value;
+    let ingVerb1OutputElement = document.getElementById('ingVerb1Output');
+    ingVerb1OutputElement.innerText = ingVerb1;
+
+    let ingVerb2InputElement = document.getElementById('ingVerb2Input');
+    let ingVerb2 = ingVerb2InputElement.value;
+    let ingVerb2OutputElement = document.getElementById('ingVerb2Output');
+    ingVerb2OutputElement.innerText = ingVerb2;
+
+    let pluralNoun3InputElement = document.getElementById('pluralNoun3Input');
+    let pluralNoun3 = pluralNoun3InputElement.value;
+    let pluralNoun3OutputElement = document.getElementById('pluralNoun3Output');
+    pluralNoun3OutputElement.innerText = pluralNoun3;
+
+    let ingVerb3InputElement = document.getElementById('ingVerb3Input');
+    let ingVerb3 = ingVerb3InputElement.value;
+    let ingVerb3OutputElement = document.getElementById('ingVerb3Output');
+    ingVerb3OutputElement.innerText = ingVerb3;
+
+    let noun3InputElement = document.getElementById('noun3Input');
+    let noun3 = noun3InputElement.value;
+    let noun3OutputElement = document.getElementById('noun3Output');
+    noun3OutputElement.innerText = noun3;
+
+    let plantInputElement = document.getElementById('plantInput');
+    let plant = plantInputElement.value;
+    let plantOutputElement = document.getElementById('plantOutput');
+    plantOutputElement.innerText = plant;
+
+    let bodyPartInputElement = document.getElementById('bodyPartInput');
+    let bodyPart = bodyPartInputElement.value;
+    let bodyPartOutputElement = document.getElementById('bodyPartOutput');
+    bodyPartOutputElement.innerText = bodyPart;
+
+    let placeInputElement = document.getElementById('placeInput');
+    let place = placeInputElement.value;
+    let placeOutputElement = document.getElementById('placeOutput');
+    placeOutputElement.innerText = place;
+
+    let ingVerb4InputElement = document.getElementById('ingVerb4Input');
+    let ingVerb4 = ingVerb4InputElement.value;
+    let ingVerb4OutputElement = document.getElementById('ingVerb4Output');
+    ingVerb4OutputElement.innerText = ingVerb4;
+
+    let adjective3InputElement = document.getElementById('adjective3Input');
+    let adjective3 = adjective3InputElement.value;
+    let adjective3OutputElement = document.getElementById('adjective3Output');
+    adjective3OutputElement.innerText = adjective3;
+
+    let numberInputElement = document.getElementById('numberInput');
+    let number = numberInputElement.value;
+    let numberOutputElement = document.getElementById('numberOutput');
+    numberOutputElement.innerText = number;
+
+    let pluralNoun4InputElement = document.getElementById('pluralNoun4Input');
+    let pluralNoun4 = pluralNoun4InputElement.value;
+    let pluralNoun4OutputElement = document.getElementById('pluralNoun4Output');
+    pluralNoun4OutputElement.innerText = pluralNoun4;
+}
+
+
